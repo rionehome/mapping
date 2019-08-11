957211755e2ea89d6269c88c8fac593aef3654dc
MiglyA <zq6295migly@gmail.com>
2019-08-11 13:43:29 +0900

    [update]launchの修正

--- scripts/mapping.py
+++ scripts/mapping.py
@@ -2,30 +2,45 @@
 # -*- coding: utf-8 -*-
 from location.srv import RegisterLocation
 import rospy
+from sound_system.srv import HotwordService
 from std_msgs.msg import String
 
 
 class Mapping:
     def __init__(self):
         self.register_topic = "/location/register_current_location"
-
+        
         rospy.init_node("mapping")
+        rospy.Subscriber("/mapping/register_place", String, self.register_callback)
         self.save_map_pub = rospy.Publisher("/location/save_location", String, queue_size=10)
-
-        self.main()
-
+        self.follow_me_control_pub = rospy.Publisher("/follow_me/control", String, queue_size=10)
+    
+    @staticmethod
+    def hot_word():
+        """
+        「hey, ducker」に反応
+        :return:
+        """
+        rospy.wait_for_service("/hotword/detect", timeout=1)
+        print "hot_word待機"
+        rospy.ServiceProxy("/hotword/detect", HotwordService)()
+    
+    def register_callback(self, msg):
+        # type:(String)->None
+        if not msg.data == "0":
+            try:
+                rospy.wait_for_service(self.register_topic, timeout=1)
+                rospy.ServiceProxy(self.register_topic, RegisterLocation)(msg.data)
+                print "登録しました。"
+            except rospy.ROSException:
+                print "Error, not find location node."
+        else:
+            self.save_map_pub.publish("mapping")
+            print "セーブ"
+    
     def main(self):
-        while True:
-            word = raw_input("place: ")
-            if not word == "0":
-                try:
-                    rospy.wait_for_service(self.register_topic, timeout=1)
-                    rospy.ServiceProxy(self.register_topic, RegisterLocation)(word)
-                except rospy.ROSException:
-                    print "Error, not find location node."
-            else:
-                self.save_map_pub.publish("mapping")
-                print "セーブ"
+        while not rospy.is_shutdown():
+            self.hot_word()
 
 
 if __name__ == '__main__':
